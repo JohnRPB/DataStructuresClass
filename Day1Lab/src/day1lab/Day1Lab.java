2014c490de27652a5dc162678c7431d7956752da
JohnRPB <baptistajohann@gmail.com>
2017-01-18 11:43:30 -0800

    fix folder structure

---
+++ Day1Lab/src/day1lab/Day1Lab.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day1lab;
+
+/**
+ *
+ * @author jbaptist
+ */
+public class Day1Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Day1LabFrame();
+        // TODO code application logic here
+    }
+    
+}
